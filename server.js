a8eaf5e3684582740b04875d3fa352ed506d08d6
nehrr <iamkapitaan@gmail.com>
2017-12-08 14:20:19 +0100

    removed force true from db sync

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
 // LET'S ROCK!
 
 // database synchronization ...
-db.sequelize.sync( { force: true } ).then(() => {
+db.sequelize.sync().then(() => {
   console.log("Database configuration success!".green);
 
   // ... and server listening
